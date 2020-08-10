da81cf9360405a2c59f7ac5aed996b539130088b
veitej <jkveitengruber@gmail.com>
2020-08-10 12:36:43 -0700

    try answer3

--- Revassess/src/sql/answer3.sql
+++ Revassess/src/sql/answer3.sql
@@ -1,1 +1,4 @@
-select user_id,username,password,first_name,last_name,role_id study_set_id,name from app_user join study_set on app_user.user_id=study_set.study_set_id where user_id=5;
+select* from app_user
+join study_set on app_user.user_id=study_set.study_set_id
+join study_set_card on study_set_card.study_set_id=study_set.study_set_id
+where user_id=5;
