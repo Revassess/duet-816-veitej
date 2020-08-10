5c211efb8042eb4438a7924dd7fcc843fed8fc62
veitej <jkveitengruber@gmail.com>
2020-08-10 11:45:11 -0700

    try the email thing again

--- Revassess/src/main/java/com/revature/tier1/CompareStrings.java
+++ Revassess/src/main/java/com/revature/tier1/CompareStrings.java
@@ -3,7 +3,7 @@ package com.revature.tier1;
 public class CompareStrings {
 
     public static boolean compareStrings(String s1, String s2){
-
+        //added this comment to repush and try to get the email
         return s1.equals(s2);
     }
 }
